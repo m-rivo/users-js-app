ef83c4656785c8ab4b7a4f81f1d4c4be06bda3df
Marycarmen <marycarmen.rivo@gmail.com>
2024-10-27 17:29:55 -0600

    Se agrega mapper a use case de load users by page

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -1,12 +1,19 @@
+import { localhostUserToModel } from "../mappers/localhost-user.mapper";
+import { User } from "../models/user";
+
 /**
  *
  * @param {Number} page
- * @returns
+ * @returns {Promise<User[]>}
  */
 export const loadUsersByPage = async (page = 1) => {
   const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`;
   const resp = await fetch(url);
-  const data = await resp.json();
+  const data = await resp?.json();
 
-  console.log(data?.data);
+  //ambas son lo mismo
+  //const users = data?.data?.map((likeUser) => localhostUserToModel(likeUser));
+  const users = data?.data?.map(localhostUserToModel);
+
+  return users;
 };
