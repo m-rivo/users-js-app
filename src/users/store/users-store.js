97af255a120d13ea72702d8021e432ff74b31efd
Marycarmen <marycarmen.rivo@gmail.com>
2024-10-27 21:25:39 -0600

    Se actualiza loadNextPage de store para que actualice el estado global

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -6,7 +6,11 @@ const state = {
 };
 
 const loadNextPage = async () => {
-  await loadUsersByPage(state.currentPage + 1);
+  const users = await loadUsersByPage(state.currentPage + 1);
+  if (users?.length === 0) return;
+
+  state.currentPage += 1;
+  state.users = users;
 };
 const loadPrevPage = async () => {
   throw new Error("No implementado");
